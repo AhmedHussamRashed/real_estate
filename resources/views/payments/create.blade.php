0440f06129a42d0f3e906114ef01fd4838a8775e
AhmedHussamRashed <127314707+AhmedHussamRashed@users.noreply.github.com>
2025-07-19 12:23:05 +0300

    update code 1

--- resources/views/payments/create.blade.php
+++ resources/views/payments/create.blade.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body class="container py-5">
-    <h2>Add New Payments </h2>
+    <h2>Send For PayPal </h2>
 
     @if(session('success'))
         <div class="alert alert-success">{{ session('success') }}</div>
